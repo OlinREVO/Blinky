f2420751d1c3ef0c4f8b6296957d7cc04055c038
Matt Wismer <mattwis86@gmail.com>
2015-02-05 22:08:41 -0500

    Add blinky and controller examples

---
+++ src/blinky.c
@@ -0,0 +1,14 @@
+#define F_CPU (1000000L)
+#include <avr/io.h>
+#include <util/delay.h>
+
+int main (void) {
+    // Set PE1 to output
+    DDRE |= _BV(PE1);
+
+    for (;;) {
+        //Toggle PE1 (pin 10)
+        PORTE ^= _BV(PE1);
+        _delay_ms(500);
+    }
+}
---
+++ src/controller.c
@@ -0,0 +1,30 @@
+#define F_CPU (1000000L)
+#include <avr/io.h>
+
+int main (void) {
+    //Enable ADC, set prescalar to 128 (slow down ADC clock)
+    ADCSRA |= _BV(ADEN) | _BV(ADPS2) | _BV(ADPS1) | _BV(ADPS0);
+    //Enable internal reference voltage
+    ADCSRB &= _BV(AREFEN);
+    //Set internal reference voltage as AVcc
+    ADMUX |= _BV(REFS0);
+    //Reads by default from ADC0
+
+    //No prescaling on PWM clock
+    TCCR0B |= _BV(CS00);
+    //Set up phase-correct PWM on OC0B
+    TCCR0A |= _BV(COM0B1) | _BV(WGM00);
+    TCCR0A &= ~_BV(COM0B0);
+    //set OC0B (PE1, pin 10) as output.
+    DDRE |= _BV(PE1);
+
+    for (;;) {
+        //Read from ADC
+        ADCSRA |=  _BV(ADSC);
+        //Wait for ADC reading
+        while(bit_is_set(ADCSRA, ADSC));
+        uint16_t reading = ADC;
+
+        OCR0B = (uint8_t) (reading >> 2);
+    }
+}
